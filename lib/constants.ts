f40e8978c14e5757c97b00e3650753d695ce5f78
Julien Genestoux <julien.genestoux@gmail.com>
2023-07-19 10:29:22 +0200

    adding token-gating

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,5 @@ export const EXAMPLE_PATH = 'blog-starter'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
   'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
+export const NETWORK = 5
+export const LOCK = '0x8053f3f9565affcc6be698fbec6aeab078854ccc'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import { WagmiConfig, createClient} from 'wagmi'
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import { ethers } from 'ethers'
+import { NETWORK } from '../lib/constants'
+
+
+const wagmiClient = createClient({
+  provider: new ethers.providers.JsonRpcProvider(`https://rpc.unlock-protocol.com/${NETWORK}`, NETWORK),
+})
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <WagmiConfig client={wagmiClient}>
+    <Component {...pageProps} />
+  </WagmiConfig>
 }
