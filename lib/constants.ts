ac5fa70075885a775d843b7fcdec6e2a95ed076d
Julien Genestoux <julien.genestoux@gmail.com>
2023-07-21 12:42:05 +0200

    updated lock address

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,4 +3,4 @@ export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
   'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
 export const NETWORK = 5
-export const LOCK = '0x8053f3f9565affcc6be698fbec6aeab078854ccc'
+export const LOCK = '0x5c92f51e7f84ec3ef0f2494ba281f74805f7e6d5'
